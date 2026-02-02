dddfc9cf79e99f2d75c28c61aa65b08ce2ea06b1
Thomas Boehm Machado <thomas.b.machado@gmail.com>
2026-02-02 19:51:40 -0300

    update: removed unnecessary content for display

--- src/shared/helpers/errors/controller_errors.py
+++
@@ -1,9 +0,0 @@
-from src.shared.helpers.errors.base_error import BaseError
-
-
-class MissingParameters(BaseError):
-    def __init__(self, message: str):
-        super().__init__(f'Parâmetro {message} não existe')
-class WrongTypeParameter(BaseError):
-    def __init__(self, fieldName: str, fieldTypeExpected: str, fieldTypeReceived: str):
-        super().__init__(f'Parâmetro {fieldName} não possui tipo correto.\n Recebido: {fieldTypeReceived}.\n Esperado: {fieldTypeExpected}')
